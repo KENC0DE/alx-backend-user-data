dbb826916e4f6d4987a319aed2059e63ef96b04e
KENC0DE <kenansameseret@gmail.com>
2024-04-11 03:41:02 +0530

    regx

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,6 @@ import re
 def filter_datum(fld: List[str], rdc: str, msg: str, sep: str) -> str:
     """
     0. Regex-ing:
-    returns the log message obfuscated:
     """
     for fl in fld:
         msg = re.sub(f'{fl}=.*?{sep}', f'{fl}={rdc}{sep}', msg)
