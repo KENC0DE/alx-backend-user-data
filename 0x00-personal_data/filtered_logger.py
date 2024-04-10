3c89e91ea2bd87dfe02bb8beea48afd10ab86597
KENC0DE <kenansameseret@gmail.com>
2024-04-11 05:13:12 +0530

    formater

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Personal data
 """
 from typing import List
+import logging
 import re
 
 
@@ -13,3 +14,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{fl}=.*?{separator}',
                          f'{fl}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Use filter_datum"""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
