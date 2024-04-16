d23dc94d3b5d3489bc8e4fb7a461be9f6c754cd7
KENC0DE <kenansameseret@gmail.com>
2024-04-17 01:40:58 +0530

    b64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -14,5 +15,19 @@ class BasicAuth(Auth):
         ath = authorization_header
         if ath and type(ath) is str and ath.startswith('Basic '):
             return ath[6:]
+
+        return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Decode base64 authorization header"""
+        a64 = base64_authorization_header
+        if a64 and type(a64) is str:
+            try:
+                code64 = base64.b64decode(a64)
+                return code64.decode('utf-8')
+            except Exception:
+                return None
 
         return None
