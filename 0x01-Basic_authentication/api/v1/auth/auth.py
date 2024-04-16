5869f188919cd1a035bb1a9c79414117f1e0e441
KENC0DE <kenansameseret@gmail.com>
2024-04-16 21:37:11 +0530

    authclass

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+The Auth Class Module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication class
+    """
+    def require_auth(self, path: str, exluded_path: List[str]) -> bool:
+        """ Return False for now"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """Returns flask request"""
+        return request
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns flask request"""
+        return request
