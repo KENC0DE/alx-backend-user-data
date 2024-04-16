537a434ac9a97a9db756b99a3344d1c5b31eedc6
KENC0DE <kenansameseret@gmail.com>
2024-04-17 01:31:21 +0530

    cut_basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """basic authentiaction class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract base64 authorizaton header"""
+        ath = authorization_header
+        if ath and type(ath) is str and ath.startswith('Basic '):
+            return ath[6:]
+
+        return None
