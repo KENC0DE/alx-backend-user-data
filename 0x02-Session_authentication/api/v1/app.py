b1662de02c8ee085375a7c49c47aa63428cd6976
KENC0DE <kenansameseret@gmail.com>
2024-04-17 22:54:19 +0530

    current user

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,9 +37,11 @@ def do_before() -> None:
     if not auth.authorization_header(request):
         abort(401)
 
-    if not auth.current_user(request):
+    current_user = auth.current_user(request)
+    if not current_user:
         abort(403)
-    request.current_user = auth.current_user(request)
+
+    request.current_user = current_user
 
 
 @app.errorhandler(404)
