07b1b101784be0220a46d1991add0dc12f43f423
KENC0DE <kenansameseret@gmail.com>
2024-04-24 02:11:08 +0530

    neet

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,11 +20,18 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, passowrd: str) -> User:
-        """Register a New User"""
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers a user in the database
+        Returns: User Object
+        """
+
         try:
-            self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
         except NoResultFound:
-            return self._db.add_user(email, _hash_password(passowrd))
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+
+            return user
+
         else:
             raise ValueError(f'User {email} already exists')
