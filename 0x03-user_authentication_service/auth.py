6ea3494f0d2fb97f74c6171e7cc6f3641887f8d4
KENC0DE <kenansameseret@gmail.com>
2024-04-24 01:41:30 +0530

    auth

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Auth Module
+"""
+import bcrypt
+
+
+def _hash_password(pwd: str) -> bytes:
+    """Returns hashed bytes of a given password"""
+    hashed_pwd = bcrypt.hashpw(pwd.encode(), bcrypt.gensalt())
+    return hashed_pwd
